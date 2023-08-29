a675ecef53bf59095b5e29bd69cfa676073b2351
Yash Shah <33934375+code-yash@users.noreply.github.com>
2023-08-29 05:33:59 +0530

    Update PalindromeTest.java

--- PalindromeTest.java
+++ PalindromeTest.java
@@ -5,30 +5,23 @@ public class PalindromeTest {
     @Test
     public void testValidPalindromes() {
         assertTrue(PalindromeChecker.isPalindrome("racecar"));
-        assertTrue(PalindromeChecker.isPalindrome("A man, a plan, a canal, Panama"));
-        assertTrue(PalindromeChecker.isPalindrome("Able was I ere I saw Elba"));
-        assertTrue(PalindromeChecker.isPalindrome("Mr. Owl ate my metal worm"));
-        assertTrue(PalindromeChecker.isPalindrome("A Toyota's a Toyota"));
-        assertTrue(PalindromeChecker.isPalindrome("Never odd or even"));
-        assertTrue(PalindromeChecker.isPalindrome("Madam, in Eden, I'm Adam"));
-        assertTrue(PalindromeChecker.isPalindrome("Was it a car or a cat I saw?"));
-        assertTrue(PalindromeChecker.isPalindrome("Step on no pets"));
-        assertTrue(PalindromeChecker.isPalindrome("Eva, can I see bees in a cave?"));
-        assertTrue(PalindromeChecker.isPalindrome("Red roses run no risk, sir, on Nurse's order"));
-        assertTrue(PalindromeChecker.isPalindrome("Ma is a nun, as I am"));
-        assertTrue(PalindromeChecker.isPalindrome("Are we not drawn onward, we few, drawn onward to new era?"));
-        assertTrue(PalindromeChecker.isPalindrome("Sir, I demand, I am a maid named Iris"));
-        assertTrue(PalindromeChecker.isPalindrome("Sir, a long-nosed man, a man no gnome nogs"));
-        assertTrue(PalindromeChecker.isPalindrome("Evil is a name of a foeman, as I live"));
+        assertTrue(PalindromeChecker.isPalindrome("rotor"));
+        assertTrue(PalindromeChecker.isPalindrome("deified"));
+        assertTrue(PalindromeChecker.isPalindrome("madam"));
+        assertTrue(PalindromeChecker.isPalindrome("level"));
+        assertTrue(PalindromeChecker.isPalindrome("civic"));
+        assertTrue(PalindromeChecker.isPalindrome("noon"));
     }
 
     @Test
     public void testInvalidPalindromes() {
-        assertFalse(PalindromeChecker.isPalindrome("hello"));
+       assertFalse(PalindromeChecker.isPalindrome("hello"));
         assertFalse(PalindromeChecker.isPalindrome("world"));
-        assertFalse(PalindromeChecker.isPalindrome("12345"));
-        assertFalse(PalindromeChecker.isPalindrome("PalindromeChecker"));
-        assertFalse(PalindromeChecker.isPalindrome("Jenkins pipeline"));
+        assertFalse(PalindromeChecker.isPalindrome("apple"));
+        assertFalse(PalindromeChecker.isPalindrome("banana"));
+        assertFalse(PalindromeChecker.isPalindrome("java"));
+        assertFalse(PalindromeChecker.isPalindrome("programming"));
+        assertFalse(PalindromeChecker.isPalindrome("computer"));
     }
 
     @Test
